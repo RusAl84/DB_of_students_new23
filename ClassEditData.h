d9dda4fcd8c79c871544372729044bb46baee70b
Aleksey <rusal@bk.ru>
2023-04-12 12:50:20 +0300

    Добавьте файлы проекта.

---
+++ ClassEditData.h
@@ -0,0 +1,22 @@
+//класс ввода данных 
+#pragma once
+#include "windows.h"
+#include <iostream>
+#include <string.h>
+#include <conio.h>
+#include <stdio.h>
+//#include "ClassMenu.h"
+
+using namespace std;
+
+class ClassEditData
+{
+private:
+	string data;
+public:
+	ClassEditData() {
+		data = "";
+	}
+
+};
+
